44091766193fbcedc379eaea8486fd618c0e70d3
saz221099 <89179947+saz221099@users.noreply.github.com>
2025-07-20 11:20:58 -0600

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
-    description('Aplicación Node JS DSL para el curso de Jenkins')
+    description('Aplicacin Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/saz221099/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('saz221099')
+            node / gitConfigEmail('carlosjosebonilla57@gmail.com')
         }
     }
     triggers {
